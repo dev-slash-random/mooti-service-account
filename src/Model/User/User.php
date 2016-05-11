1f6338c23a3fdc0a08737de039b883ad69c9f290
Ken Lalobo <lalobo@gmail.com>
2016-05-11 17:43:01 +0100

    adding db functionality

--- src/Model/User/User.php
+++ src/Model/User/User.php
@@ -8,7 +8,7 @@
 namespace Mooti\Service\Account\Model\User;
 
 use Mooti\Framework\Framework;
-//use Mooti\Validator\Validator;
+use Mooti\Validator\Validator;
 use JsonSerializable;
 
 class User implements JsonSerializable
@@ -38,6 +38,7 @@ class User implements JsonSerializable
     public function __construct($data)
     {
         $this->data = $data;
+        $this->validate();
     }
 
     public function validate()
--- src/Model/User/UserMapper.php
+++ src/Model/User/UserMapper.php
@@ -17,26 +17,82 @@ class UserMapper
         1 => [
             'id'        => 1,
             'firstName' => 'Ken',
-            'LastName'  => 'Lalobo',
+            'lastName'  => 'Lalobo',
         ],
         2 => [
             'id'        => 2,
             'firstName' => 'Joe',
-            'LastName'  => 'Bloggs',
+            'lastName'  => 'Bloggs',
         ]
     ];
 
-    public function findAll()
+    public function findOne(array $fields)
     {
-        $returnUsers = [];
-        foreach ($this->users as $userDetails) {
-            $returnUsers[] = new User($userDetails);
+        $result = $this->find($fields);
+        if (count($result['result']['count'] == 0)) {
+            throw new Exception("NOT FOUND!!");    
         }
+
+        return $result['result']['items'][0];
+    }
+
+    public function find(array $fields)
+    {
+        /*$fields = [
+            'id' => ['=', 1]
+        ];*/
+        $query = $this->createNew(Query);
+        
+        $query->setCollection('users');
+        $query->setFields($fields);
+        $query->setPage(1);
+        $query->setPerPage(10);
+        $query->setMaxPages(5);
+        $query->setSort('firstName', Query::SORT_ASC);
+        $result = $query->search();
+        $items = $result->getItems();
+
+        $returnUsers = [];
+        foreach ($items as $item) {
+            $returnUsers[] = $this->getUser($item['id']);
+        }
+
+        $result = [
+            'search' => [
+                'fields'   => $fields,
+                'page'     => $page,
+                'perPage'  => $perPage,
+                'maxPages' => $maxPages,
+                'sort'     => $sort
+            ],
+            'result' => [
+                'count' => $result->count(),
+                'items' => $returnUsers
+            ]
+        ];
         return $returnUsers;
     }
 
-    public function find($id)
+    public function getUser($id)
     {
-        return new User($this->users[$id]);
+        //nilportugues/php-sql-query-builder
+        $db = $this->get(ServiceProvider::MYSQL_DATABASE);
+        
+        $builder = createNew(MySqlBuilder::class); 
+
+        $query = $builder->select()
+            ->setTable('user')
+            ->setColumns(['id' => 'id', 'firstName' => 'first_name', 'lastName' => 'last_name'])
+            ->where()
+            ->equals('user_id', 1)
+            ->limit(1);
+
+        $this->log($builder->writeFormatted($query));
+
+        $results = $db->query($builder->write($query));
+
+        $userData = $results[0];
+
+        return new User($userData);
     }
 }
